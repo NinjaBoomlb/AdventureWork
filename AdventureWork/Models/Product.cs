179db5b2ccb28542a8634ccdf998eeeac7a87412
NinjaBoomlb <tkhatib321x@gmail.com>
2023-04-07 13:06:08 +0300

    Created Product class, ProductSubCategory class. Updated the appsettings and added the connection string of the sql server

---
+++ AdventureWork/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventureWork.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductNumber { get; set; }
+        public int MakeFlag { get; set; }
+        public int FinishedGoodsFlag { get; set; }
+        public string Color { get; set; }
+        public int SafetyStockLevel { get; set; }
+        public int ReorderPoint { get; set; }
+        public int StandardCost { get; set; }
+        public int ListPrice { get; set; }
+        public string Size { get; set; }
+        public string SizeUnitMeasureCode { get; set; }
+        public string WeightUnitMeasureCode { get; set; }
+        public float Weight { get; set; }
+        public int DaysToManufacture { get; set; }
+        public string ProductLine { get; set; }
+        public string Class { get; set; }
+        public string Style { get; set; }
+        public ProductSubcategory ProductSubcategoryID { get; set; }
+        public int ProductModelID { get; set; }
+        public DateTime SellStartDate { get; set; }
+        public DateTime SellEndDate { get; set; }
+        public DateTime DiscontinuedDate { get; set; }
+        public Guid rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+    }
+}
---
+++ AdventureWork/Models/ProductSubcategory.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventureWork.Models
+{
+    public class ProductSubcategory
+    {
+    public int ProductSubcategoryID {  get; set; }
+    public int ProductCategoryID { get; set; }
+
+    public string Name { get; set; }
+
+    public Guid rowguid { get; set; }
+
+    public DateTime ModifedDate { get; set; }
+    }
+}
