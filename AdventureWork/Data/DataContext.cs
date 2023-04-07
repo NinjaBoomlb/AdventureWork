0e4509a6b96ffe819a97e45ee388bfe40a1f7495
NinjaBoomlb <tkhatib321x@gmail.com>
2023-04-07 15:01:26 +0300

    Created the data context ( data manipulation from sql server) and added it to Program.cs

---
+++ AdventureWork/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AdventureWork.Models;
+
+namespace AdventureWork.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductInventory> ProductInventories { get; set; }
+        public DbSet<ProductPhoto> ProductPhoto { get; set; }
+        public DbSet<ProductSubcategory> ProductSubcategories { get; set; }
+        public DbSet<ProductProductPhoto> ProductProductPhoto { get; set; }
+
+        //   protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //  {
+        //      modelBuilder.Entity<ProductProductPhoto>().HasKey(p => new { p.ProductID, p.ProductPhotoID });
+        //     modelBuilder.Entity<ProductProductPhoto>()
+        //          .HasOne(pc => pc.ProductID)
+        // }
+    }
+}
