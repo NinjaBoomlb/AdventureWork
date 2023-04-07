4551f4760dc6d23789ce2eb15c667722b61d2783
NinjaBoomlb <tkhatib321x@gmail.com>
2023-04-07 14:12:45 +0300

    Created Product Category and Product Inventory

---
+++ AdventureWork/Models/ProductInventory.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventureWork.Models
+{
+    public class ProductInventory
+    {
+        public int ProductID { get; set; }
+        public int LocationID { get; set; }
+        public string Shelf { get; set; }
+        public int Bin { get; set; }
+        public int Quantity { get; set; }
+        public Guid rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+    }
+}
